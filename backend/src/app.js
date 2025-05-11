31761b3b867a85294119b91aacd3c9137d8593e3
nav-github3 <navneetlonare77@gmail.com>
2025-05-11 22:40:34 +0530

    Setting up the project

---
+++ backend/src/app.js
@@ -0,0 +1,8 @@
+import express from "express";
+import cors from "cors";
+import bcrypt from "bcrypt";
+
+const app = express(); 
+
+
+
---
+++ backend/src/index.js
@@ -0,0 +1,13 @@
+import connectDB from "./db/index.js";
+import { dbName } from "./contants.js";
+import dotenv from "dotenv";
+
+
+
+
+dotenv.config();
+connectDB();
+
+
+
+
